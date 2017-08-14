55c618f5ee51416d57e9df4ced08b787cff8aa78
Flavio <flavio_fernandes@gap.com>
2017-08-13 23:11:02 -0300

    collections

---
+++ src/com/flavio/chapter04/collections/SplQueueTest.java
@@ -0,0 +1,15 @@
+package com.flavio.chapter04.collections;
+
+public class SplQueueTest {
+    public static void main(String []args) {
+        SplQueue splQ = new SplQueue();
+        splQ.addInQueue("Harrison");
+        splQ.addInQueue("McCartney");
+        splQ.addInQueue("Starr");
+        splQ.addInQueue("Lennon");
+        splQ.printQueue();
+        splQ.removeFront();
+        splQ.removeBack();
+        splQ.printQueue();
+    }
+}
